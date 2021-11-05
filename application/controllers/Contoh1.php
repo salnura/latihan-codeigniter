fe38d4cd38dc5839262eda3e79cbdec13f148e18
salsa azizah <salsanur062@gmail.com>
2021-11-05 16:15:04 +0700

    pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Salsa Nur Aziza
+ Saya tinggal di daerah Sragen
+ olah raga yang saya sukai adalah
+ Futsal";
+ }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Salsa</li>
+        <li>Nama Belakang : Nur Aziza</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jenar, Rt 02/Rw 01, Jalan Jenar-Banyurip, Jenar, Sragen</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bandung</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Renang</li>
+    </ul>
+</section>
